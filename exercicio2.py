c8e1a2b8d312dc8cc217b0011e11e01cf3f98ed7
Gabriel <gabriel.souza21@outlook.com>
2025-04-01 14:43:36 -0300

    calculo com todos as operaçoes

---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+numero1 = int (input("insira o primeiro numero"))
+numero2 = int (input("insira o segundo numero"))
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+print(f" Sua soma {soma}\n sua subtracao {subtracao}\n sua multiplicacao {multiplicacao}\n sua divisao {divisao} ")
